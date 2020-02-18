ec32300d98237d7631cea6ddd32c15127d2f97d9
D.J. Cooper <tuttle@metafaux.com>
2020-02-18 08:06:08 -0800

    first section complete

---
+++ src/index.js
@@ -0,0 +1,59 @@
+const { GraphQLServer } = require('graphql-yoga')
+
+/**
+ * Scalar types represent the "leaves" of the query
+ * Int, Float, String, Boolean, ID
+ *
+ * typeDefs: type definitions of your schema
+ * resolvers: JS object that mirrors root fields
+ * context: object passed thru resolver chain, every
+ *  resolver can read or write to
+ *
+ * When querying an object type, required to query at
+ * least one of its fields
+ *
+ * milestone: completed "Getting Started"
+ * on to "A Simple Query"
+ * https://www.howtographql.com/graphql-js/2-a-simple-query/
+ */
+
+// 1
+const typeDefs = `
+type Query {
+    users: [User!]!
+    user(id: ID!): User
+}
+
+type Mutation {
+    createUser(name: String!): User!
+}
+
+type User {
+    id: ID!
+    name: String!
+}
+`
+
+// 2
+const resolvers = {
+    Query: {
+        info: () => null,
+    },
+}
+
+// 3
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers,
+})
+
+/**
+ * Every GraphQL schema has three special root types, these
+ * are called Query, Mutation, and Subscription.
+ * They correspond to the 3 operation types
+ * The fields on these are called root field and define
+ * the available API operations
+ *
+ */
+
+server.start(() => console.log(`Server is running on http://localhost:4000`))
